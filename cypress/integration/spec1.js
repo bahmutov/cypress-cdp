0b3c0c4eb60e0dbfebe865910e7be39ad0670a5c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-04-15 12:50:25 -0400

    update test and badge

---
+++ cypress/integration/spec1.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+it('clicks on the button', () => {
+  cy.visit('public/index.html')
+  // let the application fully load
+  cy.wait(5000)
+  cy.get('button#one').click()
+  cy.contains('#output', 'clicked')
+})
